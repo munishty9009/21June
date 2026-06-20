a73f16c0aeff9dc86fff0314570754999bf2da22
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-20 18:44:46 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,146 @@
+################################################################################
+# Root Module - Azure VM + Load Balancer
+# Standard: Enterprise-grade, modular, least-privilege
+################################################################################
+
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.90"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.47"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+
+  # Remote state - update with your storage account details
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "sttfstate<unique_suffix>"
+    container_name       = "tfstate"
+    key                  = "prod/vm-lb/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
+    virtual_machine {
+      delete_os_disk_on_deletion     = true
+      graceful_shutdown              = false
+      skip_shutdown_and_force_delete = false
+    }
+    key_vault {
+      purge_soft_delete_on_destroy    = false
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+}
+
+################################################################################
+# Resource Group
+################################################################################
+
+resource "azurerm_resource_group" "main" {
+  name     = local.resource_group_name
+  location = var.location
+  tags     = local.common_tags
+}
+
+################################################################################
+# Networking Module
+################################################################################
+
+module "networking" {
+  source = "./modules/networking"
+
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  environment         = var.environment
+  project             = var.project
+  vnet_address_space  = var.vnet_address_space
+  subnet_prefixes     = var.subnet_prefixes
+  tags                = local.common_tags
+}
+
+################################################################################
+# Security Module (NSG + Key Vault)
+################################################################################
+
+module "security" {
+  source = "./modules/security"
+
+  resource_group_name  = azurerm_resource_group.main.name
+  location             = azurerm_resource_group.main.location
+  environment          = var.environment
+  project              = var.project
+  vm_subnet_id         = module.networking.vm_subnet_id
+  allowed_source_cidrs = var.allowed_source_cidrs
+  tenant_id            = var.tenant_id
+  tags                 = local.common_tags
+
+  depends_on = [module.networking]
+}
+
+################################################################################
+# Load Balancer Module
+################################################################################
+
+module "loadbalancer" {
+  source = "./modules/loadbalancer"
+
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  environment         = var.environment
+  project             = var.project
+  lb_sku              = var.lb_sku
+  lb_frontend_port    = var.lb_frontend_port
+  lb_backend_port     = var.lb_backend_port
+  lb_protocol         = var.lb_protocol
+  health_probe_port   = var.health_probe_port
+  health_probe_proto  = var.health_probe_proto
+  tags                = local.common_tags
+}
+
+################################################################################
+# Virtual Machine Module (x5)
+################################################################################
+
+module "vms" {
+  source = "./modules/vm"
+
+  for_each = { for idx in range(var.vm_count) : format("%02d", idx + 1) => idx }
+
+  vm_name             = "${local.vm_name_prefix}-${each.key}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  environment         = var.environment
+  project             = var.project
+  vm_size             = var.vm_size
+  admin_username      = var.admin_username
+  vm_subnet_id        = module.networking.vm_subnet_id
+  lb_backend_pool_id  = module.loadbalancer.backend_pool_id
+  key_vault_id        = module.security.key_vault_id
+  os_disk_type        = var.os_disk_type
+  os_disk_size_gb     = var.os_disk_size_gb
+  source_image        = var.source_image
+  availability_zone   = tostring((each.value % 3) + 1)   # Spread across 3 AZs
+  enable_boot_diag    = var.enable_boot_diag
+  storage_account_uri = var.enable_boot_diag ? module.networking.diag_storage_uri : null
+  tags                = merge(local.common_tags, { "vm-index" = each.key })
+
+  depends_on = [module.security, module.loadbalancer, module.networking]
+}
