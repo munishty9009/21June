34bd0cbd4bb392443cb9a34072208dc3334b68cf
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-20 18:49:09 -0400

    Add Azure provider configuration for virtual machines

---
+++ provider.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  features {
+    virtual_machine {
+      delete_os_disk_on_deletion     = true
+      graceful_shutdown              = false
+      skip_shutdown_and_force_delete = false
+    }
+  }
+}
