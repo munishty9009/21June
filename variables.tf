c339ce41a516f6e01e9c7fd1992e4d4eea72513b
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-20 22:12:15 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "Azure region for all resources"
   type        = string
-  default     = "SouthEast Asia"
+  default     = "SouthIndia"
 }
 
 variable "vm_name" {
@@ -19,7 +19,7 @@ variable "vm_name" {
 variable "vm_size" {
   description = "Size/SKU of the virtual machine"
   type        = string
-  default     = "Standard_B16pls_v2"
+  default     = "Standard_B8s_v2"
 }
 
 variable "admin_username" {
