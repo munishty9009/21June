63ac5861bbff75d0b81b9a633e5ecc3691e7dbe5
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-20 21:08:43 -0400

    Remove unused required providers from provider.tf
    Remove required providers section for azuread and random.

--- provider.tf
+++ provider.tf
@@ -1,16 +1,5 @@
 terraform {
   required_version = ">= 1.5.0"
-
-  required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.47"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
-  }
 }
 
 provider "azurerm" {
