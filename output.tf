f0e291c69cead9b62b5046285ae7e89fc5d855f3
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-20 20:46:37 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,54 +1,17 @@
-vm_name
+#vm_name
 
-vm_id
-
-
-vm_private_ip
+#vm_id
 
 
-vm_public_ip
+#vm_private_ip
 
-vm_principal_id
 
-output "resource_group_name" {
-  description = "Name of the resource group"
-  value       = azurerm_resource_group.main.name
-}
+#vm_public_ip
+
+#vm_principal_id
 
 output "resource_group_name" {
   description = "Name of the resource group"
   value       = azurerm_resource_group.main.name
 }
-
-
-output "resource_group_name" {
-  description = "Name of the resource group"
-  value       = azurerm_resource_group.main.name
-}
-
-output "resource_group_name" {
-  description = "Name of the resource group"
-  value       = azurerm_resource_group.main.name
-}
-
-output "resource_group_name" {
-  description = "Name of the resource group"
-  value       = azurerm_resource_group.main.name
-}
-
-output "resource_group_name" {
-  description = "Name of the resource group"
-  value       = azurerm_resource_group.main.name
-}
-
-output "resource_group_name" {
-  description = "Name of the resource group"
-  value       = azurerm_resource_group.main.name
-}
-
-output "resource_group_name" {
-  description = "Name of the resource group"
-  value       = azurerm_resource_group.main.name
-}
-
 
