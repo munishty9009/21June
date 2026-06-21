21002b99e9fb5386d0c52e4d143c556e2bae2bd7
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-20 21:45:20 -0400

    Add Azure Resource Group resource definition

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@
 # Resource Group
 ################################################################################
 
+
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.location
