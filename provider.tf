fa9c6ad2fcf88319ae8a3763a9e63c222844e3f4
munishty9009 <66758164+munishty9009@users.noreply.github.com>
2026-06-20 20:28:33 -0400

    Update Terraform provider configurations

--- provider.tf
+++ provider.tf
@@ -1,9 +1,39 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.90"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.47"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+}
+
 provider "azurerm" {
   features {
+    resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
     virtual_machine {
       delete_os_disk_on_deletion     = true
       graceful_shutdown              = false
       skip_shutdown_and_force_delete = false
     }
+    key_vault {
+      purge_soft_delete_on_destroy    = false
+      recover_soft_deleted_key_vaults = true
+    }
   }
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
 }
+
